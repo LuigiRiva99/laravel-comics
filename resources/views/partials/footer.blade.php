5e5f53031e600288b24f8891c615facc1d9c26ad
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-05-21 14:00:01 +0200

    ADD: Layout, partials folders and home page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,23 @@
+<header>
+  <div class="container">
+    <div>
+      <ul>
+        <li>
+          <a>footer link</a>
+        </li>
+        <li>
+          <a>footer link</a>
+        </li>
+        <li>
+          <a>footer link</a>
+        </li>
+        <li>
+          <a>footer link</a>
+        </li>
+        <li>
+          <a>footer link</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</header>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+  <div class="container">
+    <div>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Blog</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+        <li>
+          <a href="">Lavora con noi</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</header>
