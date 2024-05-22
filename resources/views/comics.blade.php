2f4a866f28d2ea74e00745e3e083aca82b4c2037
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-05-22 15:59:34 +0200

    modified card style

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -8,8 +8,8 @@
             <div class="row row-gap-4">
                 @foreach ($comics as $comic)
                     <div class="col-2">
-                        <div class="card">
-                            <img src="{{$comic['thumb']}}" class="card-img-top">
+                        <div class="card ms_card">
+                            <img src="{{$comic['thumb']}}" class="card-img-top card-image">
                             <div class="card-body text-center">
                                 <p class="title">{{ $comic['title'] }}</p>
                                 <p>Serie: {{$comic['series']}}</p>
